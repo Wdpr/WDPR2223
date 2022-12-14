cd1564736cba565eaba3ea653b71089e3c5d128c
Petzr <peterjansen812@gmail.com>
2022-12-14 13:42:28 +0100

    eerste foto op de page is te zien

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
+import theaterHomePic from '../assets/theaterHomePic.jpg';
 
 export class Home extends Component {
   static displayName = Home.name;
 
   render() {
     return (
-      <div>
-        <h1>vette cool theater</h1>
-      </div>
+      <>
+        <div className="container">
+          <img src={theaterHomePic} width="1300" height="500" alt="Theater" />
+          <h3>Theater</h3>
+          <h1>Laak</h1>
+        </div>
+        
+      </>
     );
   }
 }
