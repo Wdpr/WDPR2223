afc0096fa6b1a399b003d4a876f26b286b4d556f
Pieter <pieter_delamar@hotmail.com>
2022-12-14 14:38:17 +0100

    Cards voor voorstelling gemaakt

--- ClientApp/src/components/Voorstelling/VoorstellingPage.js
+++ ClientApp/src/components/Voorstelling/VoorstellingPage.js
@@ -1,9 +1,36 @@
 import React from 'react'
+import { VoorstellingPageCard } from './VoorstellingPageCard';
+
+import cardFoto from '../../assets/guido-weijers.jpg';
+import lubachFoto from '../../assets/lubach.jpg';
 
 export class VoorstellingPage extends React.Component{
     static displayName = VoorstellingPage.name
 
-    render() {
-        return <div>Pagina voor voorstelllingen</div>
+    render(props) {
+        return (
+        <div >
+            <VoorstellingPageCard 
+                naamVanVoorstelling='Guido de legend'
+                img={cardFoto}
+                naam='Guido Weijers'
+                locatie='Theater de Laak'
+                datum='Zaterdag 12/12/2020'
+                tijd='20:00'
+                prijs='€ 20'
+                genre='Comedy'
+            />
+            <VoorstellingPageCard
+                naamVanVoorstelling='Lubach de grote' 
+                img={lubachFoto}
+                naam='Arjen Lubach'
+                locatie='Theater de Laak'
+                datum='Dinsdag 18/10/2022'
+                tijd='22:30'
+                prijs='€ 17,50'
+                genre='Comedy'
+            />
+        </div>
+        )
     }
 }
