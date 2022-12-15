4182ad8221d055b1e8545b4b863c8622a37448c2
Petzr <peterjansen812@gmail.com>
2022-12-15 19:47:49 +0100

    database opgesteld

--- Context/AccountContext.cs
+++ Context/AccountContext.cs
@@ -1,6 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using Laak.Models;
 
 namespace Laak.Context;
 
-public class AccountContext : asdf {
-    
+public class AccountContext : DbContext {
+    public AccountContext(DbContextOptions<AccountContext> options) : base(options) { }
+
+    public DbSet<Artiest> Artiesten { get; set; }
 }
