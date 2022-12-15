46fc15794e4f7c64df6c32bc3738383d8c78e7f3
Petzr <peterjansen812@gmail.com>
2022-12-15 22:15:25 +0100

    werkend controller met POST

--- Controllers/VoorstellingController.cs
+++ Controllers/VoorstellingController.cs
@@ -7,28 +7,48 @@ namespace Laak.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class VoorstellingController : ControllerBase {
-    private TheaterContext context; 
+public class VoorstellingController : ControllerBase
+{
+    private TheaterContext context;
 
-    public VoorstellingController(TheaterContext context) {
+    public VoorstellingController(TheaterContext context)
+    {
         this.context = context;
     }
 
     [HttpGet]
-    public IEnumerable<Voorstelling> GetAlleVoorstelling() {
+    public IEnumerable<Voorstelling> GetAlleVoorstelling()
+    {
         return context.Voorstellingen;
     }
 
     [HttpGet("{id}")]
-    public Voorstelling GetVoorstelling(int id) {
+    public Voorstelling GetVoorstelling(int id)
+    {
         return context.Voorstellingen.SingleOrDefault(v => v.id == id);
     }
 
     [HttpPost]
     [Route("niewuweVoorstelling")]
-    public Voorstelling PostVoorstelling(Voorstelling voorstelling) {
+    public Voorstelling PostVoorstelling(Voorstelling voorstelling)
+    {
+        Console.WriteLine("Voorstelling toegevoegd");
         context.Voorstellingen.Add(voorstelling);
         context.SaveChanges();
         return voorstelling;
     }
+    /* 
+    {
+    "Id": 1,
+    "Naam": "Lubach de Grote",
+    "Img": "img",
+    "Artiest": {},
+    "Bank": {},
+    "Zaal": {},
+    "Datum": {},
+    "Tijd": {},
+    "Prijs": 5,
+    "Genre": "Comedy"
+    }
+    */
 }
