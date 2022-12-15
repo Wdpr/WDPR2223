a6e43009a7c640af78c3cd1102f8a35a2f6da295
Pieter <pieter_delamar@hotmail.com>
2022-12-15 15:10:55 +0100

    Refactoring van de Voorstelling cards

--- ClientApp/src/components/Voorstelling/VoorstellingPageCard.js
+++ ClientApp/src/components/Voorstelling/VoorstellingPageCard.js
@@ -1,33 +1,37 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
+
+
 
 export const VoorstellingPageCard = (props) => {
     return (
         <div >
         <h1 className='pageCardTitel'>{props.naamVanVoorstelling}</h1>
             <div className= 'voorstellingCard'>
-                <img src={props.img} alt='Foto van voorstelling'/>
+                <img src={require ('../../assets/' + props.img)} alt='Foto van voorstelling'/>
                     <div className='cardDatumInfo'>
                         <ul>
-                            <li>{props.datum}</li>
                             <li>{props.tijd}</li>
                             <li>{props.locatie}</li>
+                            <li>{props.datum}</li>
                         </ul>
                     </div>
                     <div className='cardVoorstellingInfo'>
                     <ul>
                             <li>{props.naam}</li>
                             <li>{props.genre}</li>
-                            <li>{props.locatie}</li>
                         </ul>
                     </div>
                     <div className='cardInfoPrijs'>
                         <ul>
                             <li><button>Toon Info</button></li>
-                            <li>Prijs $5,00</li>
-
+                            <li>{props.prijs}</li>
                         </ul>
                     </div>
             </div>
         </div>
-        )
+        )  
 }
+
+
+//{`../../assets/${props.img}`}
