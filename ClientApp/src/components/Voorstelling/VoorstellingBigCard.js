a4b71f9b4ed7b660cc9028d45e11ba9db51edc15
Petzr <peterjansen812@gmail.com>
2022-12-19 14:50:43 +0100

    begin voor voorstelling fetch

--- ClientApp/src/components/Voorstelling/VoorstellingBigCard.js
+++ ClientApp/src/components/Voorstelling/VoorstellingBigCard.js
@@ -1,31 +1,47 @@
 import React from 'react'
 
-export const VoorstellingBigCard = (props) => {
+export async function VoorstellingBigCard(props) {
+
+    // let response = await fetch('api/Voorstelling/' + props.idnr);
+    // console.log(response)
+    // let data = response.json();
+    // console.log(data)
+
     return (
-        <div >
-        <h1 className='pageCardTitel'>{props.naamVanVoorstelling}</h1>
-            <div className= 'voorstellingCard'>
-                <img src={require ('../../assets/' + props.img)} alt='Foto van voorstelling'/>
-                    <div className='cardDatumInfo'>
-                        <ul>
-                            <li>{props.tijd}</li>
-                            <li>{props.locatie}</li>
-                            <li>{props.datum}</li>
-                        </ul>
-                    </div>
-                    <div className='cardVoorstellingInfo'>
-                    <ul>
-                            <li>{props.naam}</li>
-                            <li>{props.genre}</li>
-                        </ul>
-                    </div>
-                    <div className='cardInfoPrijs'>
-                        <ul>
-                            <li><button>Toon Info</button></li>
-                            <li>{props.prijs}</li>
-                        </ul>
-                    </div>
-            </div>
+        <div>
+            <h1>{props.naam}</h1>
+            <p>
+                {props.tijd}
+            </p>
+            <p>{props.genre}</p>
         </div>
-        )  
+    )
+
+    // return (
+    //     <div >
+    //         <h1 className='pageCardTitel'>{data.naam}</h1>
+    //         <div className='voorstellingCard'>
+    //             {/* <img src={require('../../assets/' + data.img)} alt='Foto van voorstelling' /> */}
+    //             <div className='cardDatumInfo'>
+    //                 <ul>
+    //                     <li>{data.tijd}</li>
+    //                     {/* <li>{data.locatie}</li> */}
+    //                     <li>{data.datum}</li>
+    //                 </ul>
+    //             </div>
+    //             <div className='cardVoorstellingInfo'>
+    //                 <ul>
+    //                     <li>{data.naam}</li>
+    //                     <li>{data.genre}</li>
+    //                 </ul>
+    //             </div>
+    //             <div className='cardInfoPrijs'>
+    //                 <ul>
+    //                     <li><button>Toon Info</button></li>
+    //                     <li>{data.prijs}</li>
+    //                 </ul>
+    //             </div>
+    //         </div>
+    //     </div>
+    // )
 }
