39100120da404b9a66d7a1700fc895f5c38c4dc7
Pieter <pieter_delamar@hotmail.com>
2023-01-02 18:34:24 +0100

    WijzigGegevens shell

--- ClientApp/src/Pages/MijnAccount/MijnGegevens.js
+++ ClientApp/src/Pages/MijnAccount/MijnGegevens.js
@@ -1,75 +1,195 @@
 import React from "react";
 import { useState } from "react";
 
-export function ToonMijnGevens(){
-    const dummyGebruiker = {id : 2521, 
-                            naam : "Sjaak Verkaak", 
-                            email : "blaEmail@Email.nl", 
-                            wachtwoord: "wachtwoord123",
-                            voorkeuren : ["Musical", "Comedy", "Drama"]
-                        }
+export function ToonMijnGevens() {
+    const dummyGebruiker = {
+        id: 2521,
+        naam: "Sjaak Verkaak",
+        email: "blaEmail@Email.nl",
+        wachtwoord: "wachtwoord123",
+        voorkeuren: ["Musical", "Comedy", "Drama"]
+    }
 
-    const dummyVoorstelling = {id : 1,
-                                titel : "The Lion King",
-                                beschrijving : "The Lion King is een musical gebaseerd op de gelijknamige animatiefilm uit 1994. De musical ging op 13 juni 1997 in première in het New Amsterdam Theatre in New York en werd in 2006 verplaatst naar het Minskoff Theatre. De musical is geschreven door Roger Allers en Irene Mecchi, die ook de film schreven, en is geregisseerd door Julie Taymor. De muziek is van Elton John en de teksten zijn van Tim Rice. De musical is in 2019 voor de 20e keer genomineerd voor een Tony Award.",
-                                duur : 120,
-                                genre : "Musical",
-                                }
+    const dummyVoorstelling = {
+        id: 1,
+        titel: "The Lion King",
+        beschrijving: "The Lion King is een musical gebaseerd op de gelijknamige animatiefilm uit 1994. De musical ging op 13 juni 1997 in première in het New Amsterdam Theatre in New York en werd in 2006 verplaatst naar het Minskoff Theatre. De musical is geschreven door Roger Allers en Irene Mecchi, die ook de film schreven, en is geregisseerd door Julie Taymor. De muziek is van Elton John en de teksten zijn van Tim Rice. De musical is in 2019 voor de 20e keer genomineerd voor een Tony Award.",
+        duur: 120,
+        genre: "Musical",
+    }
 
     const dummyDonatieBedrag = 500;
     const dummyWerknemer = 0;
-    const dummyAdmin = 1;
+    const dummyAdmin = 0;
 
-    return(
+    const [ToonWijzigForm, setToonWijzigForm] = useState(false);
+    const [selectedItem, setSelectedItem] = useState(null);
+
+
+    const handleClick = (event) => {
+        setSelectedItem(event.target.innerText);
+    }
+
+
+
+
+    return (
         <div>
             <div className="containervoorMijnGegevensEnMijnReserveringen">
-                <div className="mijnGegevens">
-                    <h1>Mijn gegevens</h1>
+                <div>
+                    <div className="mijnGegevens">
+                        <h1>Mijn gegevens</h1>
 
-                    <h5>Naam:</h5>
-                    <p>{dummyGebruiker.naam}</p>
+                        <h5>Naam:</h5>
+                        <p className="gebruikerGegevens">{dummyGebruiker.naam}</p>
 
-                    <h5>Email:</h5>
-                    <p>{dummyGebruiker.email}</p>
+                        <h5>Email:</h5>
+                        <p className="gebruikerGegevens">{dummyGebruiker.email}</p>
 
-                    <h5>Voorkeuren</h5>
-                    <ul>
-                        {dummyGebruiker.voorkeuren.map((item) => (
-                        <li key={item}>{item}</li>
-                        ))}
-                    </ul>
+                        <h5>Voorkeuren</h5>
+                        <ul className="gebruikerGegevens">
+                            {dummyGebruiker.voorkeuren.map((item) => (
+                                <li key={item}>{item}</li>
+                            ))}
+                        </ul>
 
-                    <h5>Totaal gedoneerd:</h5>
-                    <p>{dummyDonatieBedrag}</p>
-                    {dummyDonatieBedrag >= 1000 ? (
-                        <button className="donatieKnop">DonateursPortaal</button>) : null}
+                        <h5>Totaal gedoneerd:</h5>
+                        <p className="gebruikerGegevens">€{dummyDonatieBedrag}</p>
+                        {dummyDonatieBedrag >= 1000 ? (
+                            <button className="donatieKnop">DonateursPortaal</button>) : null}
 
-                    <button className="wijzigGegevens">Wijzig gegevens</button>
+                        <button className="wijzigGegevens" onClick={() => setToonWijzigForm(!ToonWijzigForm)}>Wijzig gegevens</button>
+                    </div>
                 </div>
 
                 <div className="mijnReserveringen">
                     <h1>Mijn reserveringen</h1>
-                    
+
                 </div>
             </div>
 
-            <div>
-            {dummyWerknemer === 1 ? (
-                <div className="werknemerPortaal">
-                <h1>Werknemer portaal</h1>
-                </div>
-            ) : null}
+            <div className="wijzigGegevensContainer">
+                {ToonWijzigForm ? (
+                    <div className="wijzigGegevensPortaal">
+                        <h1>Wat wilt u wijzigen</h1>
+
+                        <div className="knoppenContainer">
+                            <ul className="keuzeWatWijzigen">
+                                <li onClick={handleClick}>Naam</li>
+                                <li onClick={handleClick}>Email adres</li>
+                                <li onClick={handleClick}>Wachtwoord</li>
+                                <li onClick={handleClick}>Voorkeuren</li>
+                            </ul>
+                        </div>
+
+                        {selectedItem === 'Naam' && (
+                            <div className="naamDiv">
+
+                                <form className="loginFormulier" >
+                                    <div >
+                                        <label className="formFieldLabel">
+                                            Voornaam
+                                            <input className="formFieldInput" type="text" name="voorNaam" placeholder="Vul uw voornaam in"  />
+                                        </label>
+                                    </div>
+                                    <div>
+                                        <label className="formFieldLabel">
+                                            Achternaam
+                                            <input className="formFieldInput" type="Wachtwoord" name="achterNaam" placeholder="Vul uw achternaam in"  />
+                                        </label>
+                                    </div>
+                                    <div>
+                                        <button className='formFieldButton' type="submit">Wijzig</button>
+                                    </div>
+                                </form>
+
+                            </div>
+                        )}
+
+                        {selectedItem === 'Email adres' && (
+                            <div className="emailDiv">
+
+                                <form className="loginFormulier" >
+                                    <div >
+                                        <label className="formFieldLabel">
+                                            Huidig Email adres
+                                            <input className="formFieldInput" type="email" name="huidigeEmail" placeholder="Vul uw huidige E-mail in"  />
+                                        </label>
+                                    </div>
+                                    <div>
+                                        <label className="formFieldLabel">
+                                            Nieuw Email adres
+                                            <input className="formFieldInput" type="email" name="nieuweEmail" placeholder="Vul uw nieuwe E-mail in"  />
+                                        </label>
+                                    </div>
+                                    <div>
+                                        <button className='formFieldButton' type="submit">Wijzig</button>
+                                    </div>
+                                </form>
+
+                            </div>
+                        )}
+
+                        {selectedItem === 'Wachtwoord' && (
+                            <div className="wachtwoordDiv">
+
+                                <form className="loginFormulier" >
+                                    <div >
+                                        <label className="formFieldLabel">
+                                            Huidig wachtwoord
+                                            <input className="formFieldInput" type="password" name="huidigWachtwoord" placeholder="Vul uw wachtwoord in"  />
+                                        </label>
+                                    </div>
+                                    <div>
+                                        <label className="formFieldLabel">
+                                            Nieuw Wachtwoord
+                                            <input className="formFieldInput" type="password" name="nieuwWachtwoord" placeholder="Vul uw nieuw wachtwoord in"  />
+                                        </label>
+                                    </div>
+                                    <div>
+                                        <label className="formFieldLabel">
+                                            Bevestiging Wachtwoord
+                                            <input className="formFieldInput" type="password" name="bevestigingWachtwoord" placeholder="Vul opnieuw uw nieuw wachtwoord in"  />
+                                        </label>
+                                    </div>
+                                    <div>
+                                        <button className='formFieldButton' type="submit">Wijzig</button>
+                                    </div>
+                                </form>
+
+                            </div>
+                        )}
+
+                        {selectedItem === 'Voorkeuren' && (
+                            <div className="voorkeurenDiv">
+
+                                <h1>Voorkeuren</h1>
+                            </div>
+                        )}
+
+
+                    </div>
+                ) : null}
+
             </div>
 
             <div>
-            {dummyAdmin === 1 ? (
-                <div className="AdminPortaal">
-                <h1>Admin portaal</h1>
-                </div>
-            ) : null}
+                {dummyWerknemer === 1 ? (
+                    <div className="werknemerPortaal">
+                        <h1>Werknemer portaal</h1>
+                    </div>
+                ) : null}
+            </div>
+
+            <div>
+                {dummyAdmin === 1 ? (
+                    <div className="AdminPortaal">
+                        <h1>Admin portaal</h1>
+                    </div>
+                ) : null}
             </div>
         </div>
-            
+
     )
 }
 
