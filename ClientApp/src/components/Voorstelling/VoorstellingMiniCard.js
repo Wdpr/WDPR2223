9773eed9e6cf80fd6e878a93069cc652651d0e62
Petzr <peterjansen812@gmail.com>
2023-01-22 12:27:08 +0100

    fix

--- ClientApp/src/components/Voorstelling/VoorstellingMiniCard.js
+++ ClientApp/src/components/Voorstelling/VoorstellingMiniCard.js
@@ -1,24 +1,20 @@
 import React from 'react'
 import { useNavigate } from "react-router-dom";
 import img from '../../assets/guido-weijers.jpg'
-import { useNavigate } from 'react-router-dom';
 
-export function VoorstellingMiniCard ({info}) {
+export function VoorstellingMiniCard({ info }) {
     const navigate = useNavigate();
 
     function handleClick() {
         navigate('/voorstelling/' + info.id, { state: info })
     }
-    
+
     return (
-
-
-         <button className='buttonInvisible' onClick={() => handleClick()}>
-            <div  className='voorstellingMiniCard'>
+        <button className='buttonInvisible' onClick={() => handleClick()}>
+            <div className='voorstellingMiniCard'>
                 <img src={img} alt={info.naam} />
                 <p>{info.naam}</p>
             </div >
         </button>
-        
     )
 }
